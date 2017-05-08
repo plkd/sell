cc25db608548d8a9fd966d14cd560d64bfd4dd6b
plkd <209603537@qq.com>
2017-05-08 11:18:13 +0800

    food details

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,37 +1,56 @@
 <template>
-  <div class="food" v-show="showFlag" ref="food">
-    <div class="food-content">
-      <div class="image-header">
-        <img :src="food.image" alt="">
-        <div class="back" @click="hide">
-          <span class="icon-arrow_lift"></span>
+  <transition name="move">
+    <div class="food" v-show="showFlag" ref="food">
+      <div class="food-content">
+        <div class="image-header">
+          <img :src="food.image" alt="">
+          <div class="back" @click="hide">
+            <span class="icon-arrow_lift"></span>
+          </div>
         </div>
-      </div>
-      <div class="content">
-        <h1 class="title">{{food.name}}</h1>
-        <div class="detail">
-          <span class="sell-count">月售{{food.sellCount}}份</span>
-          <span class="rating">好评率{{food.rating}}%</span>
+        <div class="content">
+          <h1 class="title">{{food.name}}</h1>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="rating">好评率{{food.rating}}%</span>
+          </div>
+          <div class="price">
+            <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+          </div>
+          <div class="cartcontrol-wrapper">
+            <cartcontrol :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+            <div class="buy" v-show="!food.count || food.count===0" @click.stop.prevent="addFirst">加入购物车</div>
+          </transition>
         </div>
-        <div class="price">
-          <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+        <split></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
         </div>
-        <div class="cartcontrol-wrapper">
-          <cartcontrol :food="food"></cartcontrol>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+
         </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  import Vue from 'vue'
   import cartcontrol from '../cartcontrol/cartcontrol'
+  import split from '../split/split'
 
   export default {
     props: {
       food: Object
     },
     components: {
-      cartcontrol
+      cartcontrol,
+      split
     },
     data () {
       return {
@@ -41,9 +60,25 @@
     methods: {
       show () {
         this.showFlag = true
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.food, {
+              click: true
+            })
+          } else {
+            this.scroll.refresh()
+          }
+        })
       },
       hide () {
         this.showFlag = false
+      },
+      addFirst (event) {
+        if (!event._constructed) {
+          return
+        }
+        this.$emit('add', event.target)
+        Vue.set(this.food, 'count', 1)
       }
     }
   }
@@ -57,6 +92,10 @@
     z-index 30
     width: 100%
     background: #fff
+    &.move-enter-active, &.move-leave-active
+      transition all .2s linear
+    &.move-enter, &.move-leave-active
+      transform translate3d(100%, 0, 0)
     .image-header
       position: relative
       width: 100%
@@ -64,8 +103,81 @@
       padding-top: 100%
       img
         position: absolute
-        top:0
-        left:0
-        width:100%
-        height:100%
+        top: 0
+        left: 0
+        width: 100%
+        height: 100%
+      .back
+        position: absolute
+        top: 10px
+        left: 0
+        .icon-arrow_lift
+          display: block
+          padding: 10px
+          font-size 20px
+          color: #fff
+
+    .content
+      position: relative
+      padding: 18px
+      .title
+        font-size 14px
+        font-weight 700
+        color: rgb(7, 17, 27)
+        line-height: 14px
+      .detail
+        margin: 8px 0 18px 0
+        font-size 0
+        .sell-count, .rating
+          font-size 10px
+          color: rgb(147, 153, 159)
+          line-height 10px
+        .sell-count
+          margin-right: 12px
+      .price
+        font-size 0
+        font-weight 700
+        line-height: 24px
+        .now
+          font-size 14px
+          color: rgb(240, 20, 20)
+          vertical-align: top
+          margin-right: 8px
+        .old
+          font-size 10px
+          color: rgb(147, 153, 159)
+          text-decoration line-through
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 20
+        font-size 10px
+        color: #fff
+        box-sizing border-box
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        background: rgb(0, 160, 220)
+        text-align center
+        border-radius 12px
+        &.fade-enter-active, &.fade-leave-active
+          transition all .2s
+        &.fade-enter, &.fade-leave-active
+          opacity: 0
+
+    .info
+      padding: 18px
+      .text
+        font-size 12px
+        font-weight 200
+        color: rgb(77, 85, 93)
+        line-height: 24px
+        padding:6px 8px
+    .rating
+      padding: 18px
 </style>
